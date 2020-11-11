5e582058cc62afea8b98904bdfe2608ce2edab5e
Valerian Saliou <valerian@valeriansaliou.name>
2020-11-11 11:46:38 +0100

    Add MakAir logo
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-// plotters-conrod
-//
-// Conrod backend for Plotters
-// Copyright: 2020, Valerian Saliou <valerian@valeriansaliou.name>
-// License: MIT
-
-mod conrod;
-
-pub use conrod::ConrodBackend;
