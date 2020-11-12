929defbc0835ebaf6ea734d4a9646c812b5dbf7a
Valerian Saliou <valerian@valeriansaliou.name>
2020-11-12 12:50:57 +0100

    Fix alpha issues
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- src/conrod.rs
+++ src/conrod.rs
@@ -374,15 +374,18 @@ impl From<&BackendColor> for ConrodBackendColor {
     fn from(item: &BackendColor) -> Self {
         let ((r, g, b), a) = (item.rgb, item.alpha);
 
-        // Notice: correct alpha channel value, looks like there is a SQRT ratio between Plotters \
-        //   and Conrod color objects, where Plotters color objects seem to output the correct \
-        //   color alpha blend once rendered on screen.
-        // Warning: 'Rgba' is actually 'Srgba', this naming in Conrod is misleading.
+        // Warning: 'Rgba' is actually 'Srgba', this naming in Conrod is misleading, hence why \
+        //   we apply a transform on its alpha channel as to correct it. Looking at Conrod \
+        //   source code, it was found out that the gamma channel is not encoded in the correct \
+        //   space because 'linear gamma yields better results when doing math with colors'. \
+        //   Though, this means that the alpha value passed would render to a brighter color and \
+        //   would not blend enough with its 'Z-1' back-layer. Converting the alpha channel to \
+        //   linear fixes this color rendering issue.
         Self(conrod::color::Color::Rgba(
             r as f32 / 255.0,
             g as f32 / 255.0,
             b as f32 / 255.0,
-            (a * a) as f32,
+            Self::gamma_srgb_to_linear(a as f32),
         ))
     }
 }
@@ -391,6 +394,19 @@ impl Into<conrod::color::Color> for ConrodBackendColor {
     #[inline(always)]
     fn into(self) -> conrod::color::Color {
         self.0
+    }
+}
+
+impl ConrodBackendColor {
+    #[inline(always)]
+    pub fn gamma_srgb_to_linear(f: f32) -> f32 {
+        // See: https://en.wikipedia.org/wiki/SRGB
+        // Code from: https://docs.rs/conrod_glium/0.71.0/src/conrod_glium/lib.rs.html#310-320
+        if f <= 0.04045 {
+            f / 12.92
+        } else {
+            ((f + 0.055) / 1.055).powf(2.4)
+        }
     }
 }
 
